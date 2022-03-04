cb89ee6fd35dcab9b69f35d2478b20c3fcf2ae66
Pareeesa <parisa.faridi80@gmail.com>
2022-03-04 10:03:17 +0330

    Initial commit

---
+++ app/src/main/java/com/example/fragmentpractice/ForthFragment.kt
@@ -0,0 +1,26 @@
+package com.example.fragmentpractice
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ForthFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forth, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
